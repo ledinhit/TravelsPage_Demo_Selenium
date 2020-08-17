d70d652d89fb30e0e32ce826f8a94da0cdf3e779
ledinhit <ledinh.94d@gmail.com>
2020-08-17 09:35:17 +0700

    Test Login

---
+++ src/utilities/Links.java
@@ -0,0 +1,8 @@
+package utilities;
+
+public class Links {
+	public static final String DOMAIN = "https://www.phptravels.net/";
+	public static final String URL_LOGIN_FRONT_END = DOMAIN + "login";
+	public static final String URL_LOGIN_BACK_END = DOMAIN + "admin";
+
+}
