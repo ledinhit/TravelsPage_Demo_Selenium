ed890d3c575260180c09695c685977bb8db4a3c7
ledinhit <ledinh.94d@gmail.com>
2020-08-25 17:35:33 +0700

    check url login success

--- src/tests/LoginTravelsHomeTest.java
+++ src/tests/LoginTravelsHomeTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -25,62 +26,75 @@ public class LoginTravelsHomeTest extends HomeTravelsBaseTest {
 		loginPg.LoginHomePage("user@phptravels.com", "demouser");
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 		String url = driver.getCurrentUrl();
-		System.out.println("Link url: " + url);
-		System.out.println("Title: " + driver.getTitle());
+		System.out.println("TC_01 Link url: " + url);
+		System.out.println("TC_01 Title: " + driver.getTitle());
 		// Assert.assertTrue(dashoardPg.isDisplayed());
 	}
 
-	/*
-	 * @Test public void loginWithPassEmpty() { loginPg = new
-	 * LoginTravelsPage(driver); loginPg.LoginHomePage("user@phptravels.com", "");
-	 * driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); String
-	 * passErrMes = driver.findElement(By.cssSelector(alert)).getText();
-	 * System.out.println("TC_02 Email messenger error : " + passErrMes);
-	 * Assert.assertEquals(passErrMes, "Invalid Email or Password");
-	 * Assert.assertFalse(dashoardPg.isDisplayed());
-	 * 
-	 * }
-	 * 
-	 * @Test public void LoginWithEmailEmpty() { loginPg = new
-	 * LoginTravelsPage(driver); loginPg.LoginHomePage("", "demouser");
-	 * driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); String
-	 * emailErrMes = driver.findElement(By.cssSelector(alert)).getText();
-	 * System.out.println("TC_03 Pass messenger error : " + emailErrMes);
-	 * Assert.assertEquals(emailErrMes, "Invalid Email or Password");
-	 * Assert.assertFalse(dashoardPg.isDisplayed());
-	 * 
-	 * }
-	 * 
-	 * @Test public void LoginWithBlankField() { loginPg = new
-	 * LoginTravelsPage(driver); loginPg.LoginHomePage("", "");
-	 * driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); String
-	 * passErrMes = driver.findElement(By.cssSelector(alert)).getText();
-	 * System.out.println("TC_04 messenger error : " + passErrMes);
-	 * Assert.assertEquals(passErrMes, "Invalid Email or Password");
-	 * Assert.assertFalse(dashoardPg.isDisplayed()); }
-	 * 
-	 * @Test public void LoginWithEmailInCorrect() { loginPg = new
-	 * LoginTravelsPage(driver); loginPg.LoginHomePage("ledinh.94@gmail.com",
-	 * "demouser"); driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-	 * String passErrMes = driver.findElement(By.cssSelector(alert)).getText();
-	 * System.out.println("TC_05 messenger error : " + passErrMes);
-	 * Assert.assertEquals(passErrMes, "Invalid Email or Password");
-	 * Assert.assertFalse(dashoardPg.isDisplayed()); }
-	 * 
-	 * @Test public void LoginWithPassInCorrect() { loginPg = new
-	 * LoginTravelsPage(driver); loginPg.LoginHomePage("user@phptravels.com",
-	 * "123456"); driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-	 * String passErrMes = driver.findElement(By.cssSelector(alert)).getText();
-	 * System.out.println("TC_05 messenger error : " + passErrMes);
-	 * Assert.assertEquals(passErrMes, "Invalid Email or Password");
-	 * Assert.assertFalse(dashoardPg.isDisplayed()); }
-	 * 
-	 * @Test public void LoginWithEmailInvalid() { loginPg = new
-	 * LoginTravelsPage(driver); loginPg.LoginHomePage("ledinh.94gmail.com",
-	 * "demouser"); driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-	 * String emailErrMes = driver.findElement(By.cssSelector(alert)).getText();
-	 * System.out.println("TC_05 messenger error : " + emailErrMes);
-	 * Assert.assertEquals(emailErrMes, "Invalid Email or Password");
-	 * Assert.assertFalse(dashoardPg.isDisplayed()); }
-	 */
+	@Test
+	public void loginWithPassEmpty() {
+		loginPg = new LoginTravelsPage(driver);
+		loginPg.LoginHomePage("user@phptravels.com", "");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		String passErrMes = driver.findElement(By.cssSelector(alert)).getText();
+		System.out.println("TC_02 Messenger error: " + passErrMes);
+		Assert.assertEquals(passErrMes, "Invalid Email or Password");
+
+	}
+
+	@Test
+	public void LoginWithEmailEmpty() {
+		loginPg = new LoginTravelsPage(driver);
+		loginPg.LoginHomePage("", "demouser");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		String emailErrMes = driver.findElement(By.cssSelector(alert)).getText();
+		System.out.println("TC_03 Messenger error : " + emailErrMes);
+		Assert.assertEquals(emailErrMes, "Invalid Email or Password");
+
+	}
+
+	@Test
+	public void LoginWithBlankField() {
+		loginPg = new LoginTravelsPage(driver);
+		loginPg.LoginHomePage("", "");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		String passErrMes = driver.findElement(By.cssSelector(alert)).getText();
+		System.out.println("TC_04 Messenger error : " + passErrMes);
+		Assert.assertEquals(passErrMes, "Invalid Email or Password");
+
+	}
+
+	@Test
+	public void LoginWithEmailInCorrect() {
+		loginPg = new LoginTravelsPage(driver);
+		loginPg.LoginHomePage("ledinh.94@gmail.com", "demouser");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		String passErrMes = driver.findElement(By.cssSelector(alert)).getText();
+		System.out.println("TC_05 Messenger error : " + passErrMes);
+		Assert.assertEquals(passErrMes, "Invalid Email or Password");
+
+	}
+
+	@Test
+	public void LoginWithPassInCorrect() {
+		loginPg = new LoginTravelsPage(driver);
+		loginPg.LoginHomePage("user@phptravels.com", "123456");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		String passErrMes = driver.findElement(By.cssSelector(alert)).getText();
+		System.out.println("TC_06 Messenger error : " + passErrMes);
+		Assert.assertEquals(passErrMes, "Invalid Email or Password");
+
+	}
+
+	@Test
+	public void LoginWithEmailInvalid() {
+		loginPg = new LoginTravelsPage(driver);
+		loginPg.LoginHomePage("ledinh.94gmail.com", "demouser");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		String emailErrMes = driver.findElement(By.cssSelector(alert)).getText();
+		System.out.println("TC_07 Messenger error : " + emailErrMes);
+		Assert.assertEquals(emailErrMes, "Invalid Email or Password");
+
+	}
+
 }
